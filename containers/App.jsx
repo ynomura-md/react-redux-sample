d5b9b19dd181daf62a55665756bda6204b612203
yoshihiro.nomura <yoshihiro.nomura@supership.jp>
2016-12-04 20:59:53 +0900

    sample

--- containers/App.jsx
+++ containers/App.jsx
@@ -1,12 +1,42 @@
 import React, { Component, PropTypes } from 'react'
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import Header from '../components/Header'
-import Counter from '../components/Counter'
-import * as CounterActions from '../actions/counter'
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MyRawTheme from '../src/myThemeFile';
+import Header from '../components/Header'
 
+class App extends Component {
+  render() {
+    const { value, actions } = this.props;
+    return (
+      <div>
+        <Header />
+        {this.props.children}
+      </div>
+    )
+  }
+	static get childContextTypes() {
+    return { muiTheme: PropTypes.object.isRequired };
+  }
+  getChildContext(){
+    return {  muiTheme: getMuiTheme(MyRawTheme)};
+  }
+}
+function mapStateToProps(state) {
+    return {
+        };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        };
+}
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
+
+/*
 class App extends Component {
   render() {
     const { value, actions } = this.props;
@@ -44,3 +74,4 @@ export default connect(
   mapStateProps,
   mapDispatchToProps
 )(App)
+*/
--- src/index.jsx
+++ src/index.jsx
@@ -2,19 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { Router, browserHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
 
-import App from '../containers/App'
+//import App from '../containers/App'
 import configureStore from '../store/configureStore'
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import routes from './routes'
 
 injectTapEventPlugin()
 
-const store = configureStore()
+const store = configureStore(browserHistory, window.__initialState__)
+
+const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={history} routes={routes} />
   </Provider>,
   document.getElementById("root")
 )
